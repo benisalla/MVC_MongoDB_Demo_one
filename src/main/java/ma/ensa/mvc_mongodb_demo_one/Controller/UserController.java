3603db040441ff370a1f04d51d6bbbc5af663ab2
ismail ben alla <ismailbenalla52@gmail.com>
2023-04-14 09:55:08 +0000

    last commit in xalah

--- src/main/java/ma/ensa/mvc_mongodb_demo_one/Controller/UserController.java
+++ src/main/java/ma/ensa/mvc_mongodb_demo_one/Controller/UserController.java
@@ -5,14 +5,17 @@ import ma.ensa.mvc_mongodb_demo_one.Beans.User;
 import ma.ensa.mvc_mongodb_demo_one.Dao.IUserMongoDBRepo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
 
-import javax.swing.text.Document;
+import java.util.Collection;
 import java.util.List;
 
 @Controller
@@ -23,13 +26,26 @@ public class UserController {
     @GetMapping(path = "/index")
     public String index(Model userModel,
                         @RequestParam(name="page",defaultValue="0")int page,
-                        @RequestParam(name="size",defaultValue="10")int size){
+                        @RequestParam(name="size",defaultValue="6")int size){
         Page<User> usersPage = userMongoDBRepo.findAll(PageRequest.of(page,size));
         userModel.addAttribute("users",usersPage.getContent());
         userModel.addAttribute("currentPage",page);
         userModel.addAttribute("size",size);
         userModel.addAttribute("pages",new int[usersPage.getTotalPages()]);
+        return "index";
+    }
 
+    @PostMapping(path = "index")
+    public String PostIndex(Model userModel,
+                        @RequestParam(name="page",defaultValue="0")int page,
+                        @RequestParam(name="size",defaultValue="6")int size,
+                        @RequestParam(name="hint",defaultValue="")String hint){
+        hint = hint.strip();
+        Page<User> usersPage = userMongoDBRepo.findByFullnameContaining(hint,PageRequest.of(page,size));
+        userModel.addAttribute("users",usersPage.getContent());
+        userModel.addAttribute("currentPage",page);
+        userModel.addAttribute("size",size);
+        userModel.addAttribute("pages",new int[usersPage.getTotalPages()]);
         return "index";
     }
 
@@ -63,12 +79,21 @@ public class UserController {
     }
 
     @PostMapping(path = "/delete")
-    public String postDelete(@RequestParam(name="id") Long id){
+    public RedirectView postDelete(@RequestParam(name="id") Long id){
         User user = userMongoDBRepo.findById(id).orElse(null);
+        final RedirectView redirectView = new RedirectView("index");
         if(user == null){
-            return "index";
+            return redirectView;
         }
         userMongoDBRepo.delete(user);
-        return "index";
+        return redirectView;
+    }
+
+    @GetMapping("/add")
+    public String getAdd(Model userModel){
+        List<User> users = userMongoDBRepo.findAll();
+        Long id = users.get(users.size()-1).getId() + 1;
+        userModel.addAttribute("id", id);
+        return "add";
     }
 }
--- src/main/java/ma/ensa/mvc_mongodb_demo_one/Dao/IUserMongoDBRepo.java
+++ src/main/java/ma/ensa/mvc_mongodb_demo_one/Dao/IUserMongoDBRepo.java
@@ -1,7 +1,12 @@
 package ma.ensa.mvc_mongodb_demo_one.Dao;
 
 import ma.ensa.mvc_mongodb_demo_one.Beans.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface IUserMongoDBRepo extends MongoRepository<User, Long> {
+    public Page<User> findByFullnameContaining(String hint, PageRequest page);
 }
